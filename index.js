e59842c8b275d26235c4e42e71fac7f75f2a037e
abeprincec <abeprincec@gmail.com>
2018-06-08 02:15:27 +0000

    recursively add arrays

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+	return array.reduce((acc, curr) => {
+		return acc + (curr instanceof Array ? sumItems(curr) : curr);
+	}, 0);
 }
+
+console.log(sumItems([1, 2, 3, 4, 5]));
 
 module.exports = sumItems;
